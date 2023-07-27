c4be4deb58bde0d5f427469824bfea4b350b8506
Celso Machado <celsobarrosmachado@gmail.com>
2023-07-27 21:22:25 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        List <Produtos> listpo = new ArrayList<Produtos>();
+        listpo.add(new Produtos("Geladeira 1", "marca", 4));
+        listpo.add(new Produtos("TV 1", "marca", 2));
+        listpo.add(new Produtos("Geladeira 2", "marca", 3));
+        listpo.add(new Produtos("TV 1", "marca", 2));
+        listpo.add(new Produtos("Geladeira 3", "marca", 2));
+        listpo.add(new Produtos("TV 1", "marca", 2));
+        listpo.add(new Produtos("TV 1", "marca", 2));
+        listpo.add(new Produtos("Geladeira 4", "marca", 1));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        List <Produtos> listgela = new ArrayList<Produtos>();
+
+
     }
 }
---
+++ src/Produtos.java
@@ -0,0 +1,37 @@
+public class Produtos {
+
+    private String nome;
+    private String marca;
+
+    private int preco;
+
+    public Produtos(String nome, String marca, int preco) {
+        this.nome = nome;
+        this.marca = marca;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+}
